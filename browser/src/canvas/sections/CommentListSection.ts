Rash419/online

commit d47f88d2c09838b81e448a01eeb0ea9995330871

writer: enable @mention for document in mobileview
- also fix fullscreen contentcontrol dropdown and mention
  popup, regression from f4dc72969689dcfd1fef5d71b138b432e1238707

Signed-off-by: Rashesh <rashesh.padia@collabora.com>
Change-Id: Ic2bc0691e95d8a82ec877d36225b9ace882e7e44

browser/src/canvas/sections/CommentListSection.ts
@@ -470,6 +470,7 @@ export class CommentSection extends app.definitions.canvasSectionObject {
 				layoutstyle: 'end'
 			},
 		]);
+		json.isPopupFullscreen = true;
 
 		var cancelFunction = function() {
 			this.cancel(comment);

browser/src/control/Control.MobileWizardWindow.js
@@ -93,7 +93,7 @@ L.Control.MobileWizardWindow = L.Control.extend({
 			this.mobileWizard.addClass('menuwizard');
 		if (this.isFunctionMenu)
 			this.mobileWizard.addClass('funcwizard');
-		if (this.isPopup)
+		if (this.isPopup && this.isPopupFullscreen)
 			this.mobileWizard.addClass('popup');
 		if (this.isSnackBar)
 			this.mobileWizard.addClass('snackbar');
@@ -523,6 +523,7 @@ L.Control.MobileWizardWindow = L.Control.extend({
 
 			this._reset();
 			this.isPopup = isPopupJson;
+			this.isPopupFullscreen = data.isPopupFullscreen;
 
 			this._showWizard();
 			if (this.map._docLayer && !this.map._docLayer.isCalc()) {

browser/css/mobilewizard.css
@@ -1233,20 +1233,24 @@ label.disabled {
 	display: none;
 }
 
-[id$='contentControlModalpopup'].mobile-wizard {
+[id$='contentControlModalpopup'].mobile-wizard, [id$='mentionPopup'].mobile-wizard {
 	display: flex;
 	flex: 1;
 	align-self: stretch;
 }
 
-#contentControlList.mobile-wizard {
+#contentControlList.mobile-wizard, #mentionPopupList.mobile-wizard {
 	margin: 0;
 	border-color: transparent;
 	width: 100%;
 }
 
-#contentControlList.mobile-wizard li:not(:last-child) {
+#contentControlList.mobile-wizard li:not(:last-child), #mentionPopupList.mobile-wizard li:not(:last-child) {
 	border-bottom: 1px solid var(--color-border-lighter);
+}
+
+#contentControlList.mobile-wizard .ui-treeview-body, #mentionPopupList.mobile-wizard .ui-treeview-body {
+	max-height: none;
 }
 
 .unotoolbutton.mobile-wizard .unobutton img{

browser/src/control/AutoCompletePopup.ts
@@ -32,6 +32,7 @@ abstract class AutoCompletePopup {
 	protected newPopupData: PopupData;
 	protected data: MessageEvent<any>;
 	protected popupId: string;
+	protected isMobile: boolean;
 
 	constructor(popupId: string, map: ReturnType<typeof L.map>) {
 		this.map = map;
@@ -53,6 +54,8 @@ abstract class AutoCompletePopup {
 			clickToClose: '_POPOVER_',
 			id: this.popupId,
 		} as PopupData;
+
+		this.isMobile = (<any>window).mode.isMobile();
 
 		this.onAdd();
 
@@ -112,7 +115,8 @@ abstract class AutoCompletePopup {
 	}
 
 	sendJSON(data: any): void {
-		this.map.fire('jsdialog', {
+		const fireEvent = this.isMobile ? 'mobilewizard' : 'jsdialog';
+		this.map.fire(fireEvent, {
 			data: data,
 			callback: this.callback.bind(this),
 		});

browser/src/control/Control.UIManager.js
@@ -252,7 +252,6 @@ L.Control.UIManager = L.Control.extend({
 
 			this.map.sidebar = JSDialog.Sidebar(this.map, {animSpeed: 200});
 
-			this.map.mention = L.control.mention(this.map);
 			this.map.formulaautocomplete = L.control.formulaautocomplete(this.map);
 			this.map.formulausage = L.control.formulausage(this.map);
 		}
@@ -381,6 +380,8 @@ L.Control.UIManager = L.Control.extend({
 			var showResolved = this.getBooleanDocTypePref('ShowResolved', true);
 			if (showResolved === false || showResolved === 'false')
 				this.map.sendUnoCommand('.uno:ShowResolvedAnnotations');
+
+			this.map.mention = L.control.mention(this.map);
 		}
 
 		if (this.map.isPresentationOrDrawing() && (isDesktop || window.mode.isTablet())) {

commit 60e8a8a6f88e3f10880924cf83218196dcadd254

fix: 'About dialog' is not fullscreen
- regression from d47f88d2c09838b81e448a01eeb0ea9995330871
- Not just comment modal there are lot of different dialog that are
fullscreen

Signed-off-by: Rashesh <rashesh.padia@collabora.com>
Change-Id: Icf1399e77e8582c1a186977edb4c01450e43527d

browser/src/canvas/sections/CommentListSection.ts
@@ -496,7 +496,6 @@ export class CommentSection extends app.definitions.canvasSectionObject {
 				layoutstyle: 'end'
 			},
 		]);
-		json.isPopupFullscreen = true;
 
 		var cancelFunction = function() {
 			this.cancel(comment);

browser/src/canvas/sections/ContentControlSection.ts
@@ -300,7 +300,8 @@ export class ContentControlSection extends app.definitions.canvasSectionObject {
 			'cancellable': true,
 			'popupParent': '_POPOVER_',
 			'clickToClose': '_POPOVER_',
-			'id': 'contentControlModalpopup'
+			'id': 'contentControlModalpopup',
+			'isPopupPartialScreen': true
 		};
 
 		var entries = [];

browser/src/control/Control.MobileWizardWindow.js
@@ -93,7 +93,7 @@ L.Control.MobileWizardWindow = L.Control.extend({
 			this.mobileWizard.addClass('menuwizard');
 		if (this.isFunctionMenu)
 			this.mobileWizard.addClass('funcwizard');
-		if (this.isPopup && this.isPopupFullscreen)
+		if (this.isPopup && !this.isAutoCompletePopup && !this.isPopupPartialScreen)
 			this.mobileWizard.addClass('popup');
 		if (this.isSnackBar)
 			this.mobileWizard.addClass('snackbar');
@@ -523,7 +523,8 @@ L.Control.MobileWizardWindow = L.Control.extend({
 
 			this._reset();
 			this.isPopup = isPopupJson;
-			this.isPopupFullscreen = data.isPopupFullscreen;
+			this.isAutoCompletePopup = data.isAutoCompletePopup;
+			this.isPopupPartialScreen = data.isPopupPartialScreen;
 			this.persistKeyboard = data.persistKeyboard;
 
 			this._showWizard();